c1a5b49cd69adae881be76e06ad9b8876dbc82b8
RutaMat <79446887+RutaMat@users.noreply.github.com>
2021-03-29 10:07:50 +0300

    Main-home

--- js/main-home.js
+++ js/main-home.js
@@ -1,9 +1,14 @@
 // IMPORT
 // hero
+// (import {clock} from '';)
+// (import {form} from '';)
 // main
 // footer
 
 // FUNKCIJU PANAUDOJIMAS
 // hero
+// (clock();)
+// (form('subscribe');)
 // main
+// (form('message');)
 // footer
